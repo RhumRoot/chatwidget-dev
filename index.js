bd618e698f5d15669d7bfd28f752a6469c06a043
RhumRoot <34861553+RhumRoot@users.noreply.github.com>
2019-05-14 14:52:54 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+<script>"use strict";
+    window.addEventListener("load", function () { var script = document.createElement('script'); script.src = 'https://chatwidget-dev-dot-activechat-200215.appspot.com/cdn.js'; script.id = 'ACCW_CDN'; document.getElementsByTagName('head')[0].appendChild(script);
+    document.querySelector('#ACCW_CDN').onload = function () {
+    ACCW.setup("6577aca2-bea1-492b-aff8-e420064a3a1a");};
+}, false);
+</script>
